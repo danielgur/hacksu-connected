5f8a0045addddaa47f133f80bc32cd1a8a769ee1
danielgur <danielgur2105@gmail.com>
2012-11-17 18:41:50 -0500

    merge

--- backend-storarge/connected/connected/views.py
+++ backend-storarge/connected/connected/views.py
@@ -5,12 +5,27 @@ from app_connected.models import Member, Login
 def index(request):
     return render_to_response('app_connected/index.html')
 
+def login(request):
+     m = Member.objects.get(username=request.POST['username'])
+     if m.password == request.POST['password']:
+        request.session['member_id'] = m.id
+        return render_to_response('app_connected/menu.html')
+     else:
+        return HttpResponse("Your username and password didn't match.")
+
 def loop(request, member_id):
-    members = Member.objects.get(id=member_id)
+    members = Member.objects.get(id=request.session['member_id'])
     return render_to_response('app_connected/loop.html', {'members':members})
 
 def menu(request):
     return render_to_response('app_connected/menu.html')
 
 def bump(request):
-    return render_to_response('app_connected/bump.html')
+    return render_to_response('app_connected/bump.html', {"member_id":request.session['member_id']})
+
+def logout(request):
+    try:
+        del request.session['member_id']
+    except KeyError:
+        pass
+    return HttpResponse("You're logged out.")
