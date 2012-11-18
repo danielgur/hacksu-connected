f9bcad08c29aca054d8e612c72c4338462e2e34a
andrewkelley <andrew.kelley@codesummit.com>
2012-11-18 15:54:11 -0500

    new changes

--- backend-storarge/connected/connected/views.py
+++ backend-storarge/connected/connected/views.py
@@ -4,34 +4,23 @@ from django.shortcuts import render_to_response
 from app_connected.models import Member, Login
 
 def index(request):
-    return render_to_response('app_connected/login.html')
+    return render_to_response('app_connected/index.html')
 
 def login(request):
-     c = {}
-     c.update(csrf(request))
-     m = Login.objects.get(username=request.POST['username'])
-     if m.password == request.POST['password']:
-        request.session['member_id'] = m.id
-        return render_to_response('app_connected/menu.html', c)
+     if request == 'POST':
+        return render_to_response('app_connected/menu.html')
      else:
-        return render_to_response('app_connected/login.html', c)
+        return render_to_response('app_connected/login.html')
 
 def loop(request, member_id):
-    members = Member.objects.get(id=request.session['member_id'])
+    members = Member.objects.get(id=member_id)
     return render_to_response('app_connected/loop.html', {'members':members})
 
 def menu(request):
     return render_to_response('app_connected/menu.html')
 
 def bump(request):
-    return render_to_response('app_connected/bump.html', {"member_id":1})
-
-def logout(request):
-    try:
-        del request.session['member_id']
-    except KeyError:
-        pass
-    return HttpResponse("You're logged out.")
+    return render_to_response('app_connected/bump.html', {"member_id": 1})
 
 def myinfo(request, member_id):
     me = Member.objects.get(id=member_id)
