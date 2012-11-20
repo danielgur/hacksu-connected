fd16739daa342d677575b087ce2050d547e35621
andrewkelley <andrew.kelley@codesummit.com>
2012-11-20 16:55:03 -0500

    updated views.py

--- backend-storarge/connected/app_connected/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render_to_response
-from app_connected.models import Member, Login
-
-def index(request):
-    members = Member.objects.all()
-    login = Login.objects.all()
-    return render_to_response('app_connected/index.html', {'members': members, 'login': login})# Create your views here.
