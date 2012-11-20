da41e452b8e27b9e3cfd545c4972550773fe20c1
andrewkelley <andrew.kelley@codesummit.com>
2012-11-20 14:34:57 -0500

    updated forms.py for Registration form

--- backend-storarge/connected/app_connected/forms.py
+++ backend-storarge/connected/app_connected/forms.py
@@ -1,7 +1,34 @@
-from models import Member, Login
-from django.db import models
+from django import forms
+from django.contrib.auth.models import User
 from django.forms import ModelForm
+from app_connected.models import Member
 
-class LoginForm(ModelForm):
+
+class RegistrationForm(ModelForm):
+    username = forms.CharField(label = (u'User Name'))
+    email = forms.EmailField(label = (u'Email Address'))
+    password = forms.CharField(label = (u'Password'),
+            widget=forms.PasswordInput(render_value=False))
+    password1 = forms.CharField(label=(u'Verify Password'),
+            widget=forms.PasswordInput(render_value=False))
+
     class Meta:
-        model = Login
+        model = Member
+        exclude = ('user',)
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        try:
+            User.objects.get(username=username)
+        except User.DoesNotExist:
+            return username
+        raise forms.ValidationError("That username is already taken, please try
+        a new one!")
+
+    def clean(self):
+        if self.cleaned_data['password'] != self.cleaned_data['password1']:
+            raise Forms.ValidationError("The passwords you input did not match.
+            Please try again.")
+        return self.cleaned_data
+
+
